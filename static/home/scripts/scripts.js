3b5ffb9608af019d228fc6aefc3bcaf136ef340f
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-09-27 12:49:16 -0300

    feat: add loader

--- static/home/scripts/scripts.js
+++ static/home/scripts/scripts.js
@@ -1,4 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    var images = document.images,
+        totalImages = images.length,
+        imagesLoaded = 0;
+
+    // Verifica se todas as imagens foram carregadas
+    function imageLoaded() {
+        imagesLoaded++;
+        if (imagesLoaded === totalImages) {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("page-content").style.display = "block";
+        }
+    }
+
+    // Verifica o carregamento das imagens
+    for (var i = 0; i < totalImages; i++) {
+        if (images[i].complete) {
+            imageLoaded();
+        } else {
+            images[i].addEventListener("load", imageLoaded);
+            images[i].addEventListener("error", imageLoaded); // Contabiliza erros também
+        }
+    }
+    
     var audio = document.getElementById('background-audio');
     audio.volume = 0.5;
     var button = document.getElementById('audio-control-button');
@@ -11,9 +34,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         if (audio.paused) {
-            audio.play().catch(function(error) {
+            audio.play().catch(function (error) {
                 console.log('Autoplay foi bloqueado. Tentando reproduzir após interação do usuário.');
             });
         } else {
@@ -23,14 +46,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Tentar reproduzir o áudio automaticamente
-    audio.play().catch(function(error) {
+    audio.play().catch(function (error) {
         console.log('Autoplay foi bloqueado. Tentando reproduzir após interação do usuário.');
     });
 
     // Atualizar o botão inicialmente
     updateButton();
 
-    document.addEventListener('click', function() {
+    document.addEventListener('click', function () {
         audio.play();
         button.textContent = '⏸️';
     }, { once: true });
