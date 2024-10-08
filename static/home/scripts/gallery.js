d3d7b21315606a7a217711588ddf60e69c7e3018
Leonardo Costa <leonardoabreu1212@gmail>
2024-10-07 22:10:38 -0300

    feat: add animation when switching images on gallery

--- static/home/scripts/gallery.js
+++ static/home/scripts/gallery.js
@@ -16,16 +16,43 @@ images.forEach((image, index) => {
     });
 });
 
+function changeImage(direction) {
+    // Add fade-out class to start fade-out animation
+    modalImage.classList.add('fade-out');
+
+    // Wait for the fade-out animation to finish
+    modalImage.addEventListener('animationend', function handler() {
+        // Remove the event listener to prevent multiple triggers
+        modalImage.removeEventListener('animationend', handler);
+
+        // Update the current image index
+        if (direction === 'next') {
+            currentImage = (currentImage + 1) % images.length;
+        } else if (direction === 'prev') {
+            currentImage = (currentImage - 1 + images.length) % images.length;
+        }
+
+        // Update the image source and title
+        var titleElement = images[currentImage].querySelector('.u-text-3');
+        modalTitle.textContent = titleElement ? titleElement.textContent : 'Foto';
+        modalImage.src = images[currentImage].querySelector('img').src;
+
+        // Remove the fade-out class and add fade-in class
+        modalImage.classList.remove('fade-out');
+        modalImage.classList.add('fade-in');
+
+        // Remove the fade-in class after the animation completes
+        modalImage.addEventListener('animationend', function handler2() {
+            modalImage.removeEventListener('animationend', handler2);
+            modalImage.classList.remove('fade-in');
+        });
+    });
+}
+
 previousButton.addEventListener('click', () => {
-    currentImage = currentImage === 0 ? images.length - 1 : currentImage - 1;
-    var titleElement = images[currentImage].querySelector('.u-text-3');
-    modalTitle.textContent = titleElement ? titleElement.textContent : 'Foto';
-    modalImage.src = images[currentImage].querySelector('img').src;
+    changeImage('prev');
 });
 
 nextButton.addEventListener('click', () => {
-    currentImage = currentImage === images.length - 1 ? 0 : currentImage + 1;
-    var titleElement = images[currentImage].querySelector('.u-text-3');
-    modalTitle.textContent = titleElement ? titleElement.textContent : 'Foto';
-    modalImage.src = images[currentImage].querySelector('img').src;
+    changeImage('next');
 });
